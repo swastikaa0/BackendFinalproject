196d6ffcce779e84e5c0beeff4bd57f0c618d714
swastikaa0 <swastikadhungana2004@gmail.com>
2026-06-09 17:00:13 +0545

    dtos  folder

---
+++ src/dtos/user.dtos.ts
@@ -0,0 +1,19 @@
+import { z } from "zod";
+import { UserSchema } from "../types/user.types";
+
+// Create a DTO for creating a user
+// export const CreateUserDTO = UserSchema.omit({ role: true });
+export const CreateUserDTO = UserSchema.pick({
+    firstName: true,
+    lastName: true,
+    email: true,
+    username: true,
+    password: true
+});
+export type CreateUserDTO = z.infer<typeof CreateUserDTO>;
+
+export const LoginUserDTO = UserSchema.pick({
+    email: true,
+    password: true
+});
+export type LoginUserDTO = z.infer<typeof LoginUserDTO>;
