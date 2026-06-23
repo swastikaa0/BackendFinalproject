ac467c8af23535b6b66a2fd7d2b5566a44349311
swastikaa0 <swastikadhungana2004@gmail.com>
2026-06-23 16:40:19 +0545

    upload routes addded

--- src/routes/upload.route.ts
+++ src/routes/upload.route.ts
@@ -1,26 +1,32 @@
-import { Router } from "express";
-import { uploads } from "../middlewares/upload.middleware";
-import { HttpException } from "../exception/http-exception";
-import { ApiResponseHelper } from "../utlis/apihelper.util";
-const router = Router();
+// import { Router ,Request,Response} from "express";
+// import { uploads } from "../middlewares/upload.middleware";
+// import { HttpException } from "../exception/http-exception";
+// import { ApiResponseHelper } from "../utlis/apihelper.util";
 
-// Single file upload
-router.post("/upload", 
+
+// interface UploadRequest extends Request {
+//   file?: Express.Multer.File;
+// }
+
+// const router = Router();
+
+// // Single file upload
+// router.post("/upload", 
   
-  uploads.single("file"), (req, res) => {
-  try {
-    if (!req.file) {
-      throw new HttpException(400, "No file uploaded");
-    }
-    req.file.path = "/uploads/"+ req.file.filename;// set file path for response 
-    return ApiResponseHelper.success(
-      res,
-      req.file,
-      "File uploaded successfully",
-    );
-  } catch (error) {
-    return ApiResponseHelper.error(res, error);
-  }
-});
+//   uploads.single("file"), (req, res) => {
+//   try {
+//     if (!req.file) {
+//       throw new HttpException(400, "No file uploaded");
+//     }
+//     req.file.path = "/uploads/"+ req.file.filename;// set file path for response 
+//     return ApiResponseHelper.success(
+//       res,
+//       req.file,
+//       "File uploaded successfully",
+//     );
+//   } catch (error) {
+//     return ApiResponseHelper.error(res, error);
+//   }
+// });
 
-export default router
+// export default router
